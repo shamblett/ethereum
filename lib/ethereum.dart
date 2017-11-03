3f06aaff7b2d95de48f3250364e48f78401031d8
Steve Hamblett <steve.hamblett@linux.com>
2017-11-03 11:43:05 +0000

    Initial structure

---
+++ lib/ethereum.dart
@@ -0,0 +1,11 @@
+/*
+ * Package : Ethereum
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 03/11/2017
+ * Copyright :  S.Hamblett
+ */
+
+library ethereum;
+
+import "package:json_rpc_2/json_rpc_2.dart";
+import "package:json_rpc_2/error_code.dart";
---
+++ test/ethereum_base_test.dart
@@ -0,0 +1,9 @@
+/*
+ * Package : Ethereum
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 03/11/2017
+ * Copyright :  S.Hamblett
+ */
+
+import 'package:ethereum/ethereum.dart';
+import 'package:test/test.dart';
