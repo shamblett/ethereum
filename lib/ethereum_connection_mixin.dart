856d149d066ee34eefc7b66636c5eed80d8ba06b
Steve Hamblett <steve.hamblett@linux.com>
2017-11-08 15:44:17 +0000

    Ethereum client

--- lib/ethereum_connection_mixin.dart
+++ lib/ethereum_connection_mixin.dart
@@ -8,34 +8,53 @@
  */
 
 abstract class EthereumConnectionMixin {
+  /// Default port
+  static const int defaultPort = 8545;
 
   /// Connection parameters
-  int port = 8545;
+  int port = defaultPort;
   String uri;
   String scheme;
 
   /// Connect using a host string of the form http://thehost.com:1234,
-  /// port is otional.
-  void connectString(String uri) {
-
+  /// port is optional.
+  void connectString(String hostname) {
+    if (hostname == null) {
+      throw new ArgumentError.notNull(
+          "Ethereum::connectString - hostname is null");
+    }
+    final Uri uri = new Uri.dataFromString(hostname);
+    _validateUri(uri);
   }
 
   /// Connect using a URI, port is optional
   void connectUri(Uri uri) {
-
+    if (uri == null) {
+      throw new ArgumentError.notNull("Ethereum::connectUri - uri is null");
+    }
+    _validateUri(uri);
   }
 
   /// Connect by explicitly setting the connection parameters
-  void connectParameters() {
-
+  void connectParameters(String scheme, String hostname, [int port]) {
+    if (hostname == null) {
+      throw new ArgumentError.notNull(
+          "Ethereum::connectParameters - hostname is null");
+    }
+    if (scheme == null) {
+      throw new ArgumentError.notNull(
+          "Ethereum::connectParameters - scheme is null");
+    }
+    int uriPort = defaultPort;
+    if (port != null) {
+      uriPort = port;
+    }
+    final Uri uri = new Uri(scheme: scheme, host: hostname, port: uriPort);
+    _validateUri(uri);
   }
 
-  void _validateUri(dynamic uri) {
-
-
-  }
+  void _validateUri(Uri uri) {}
 
   /// Internal connect, must be overridden in a server/browser class
   void _connect();
-
 }
