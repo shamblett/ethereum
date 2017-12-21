5bf6513ca0453d31ee0f077a945b14ad8c11d709
Steve Hamblett <steve.hamblett@linux.com>
2017-12-21 10:29:44 +0000

    Add browser tests

--- test/test_configuration.dart
+++ test/test_configuration.dart
@@ -5,6 +5,11 @@
  * Copyright :  S.Hamblett
  */
 
+/// Start geth as follows :-
+/// geth --unlock 0xd10de988e845d33859c3f96c7f1fc723b7b56f4c --rpc --shh
+/// The account above is the test account defined below, you must first create this account,
+/// see the geth documentation for details.
+///
 /// Test configuration options
 class TestConfiguration {
   static const int defaultAccount = 0xd10de988e845d33859c3f96c7f1fc723b7b56f4c;
