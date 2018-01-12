eff8829e487d07ac6b2479079aef939918d6beec
Steve Hamblett <steve.hamblett@linux.com>
2018-01-12 10:23:06 +0000

    Add example, prep for release

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ethereum
-An ethereum RPC 2 client for Dart
+[![Build Status](https://travis-ci.org/shamblett/ethereum.svg?branch=master)](https://travis-ci.org/shamblett/ethereum)
+
+An ethereum RPC 2 client for Dart.
+
+This package provides an API client to access the Ethereum JSON RPC API documented [here](https://github.com/ethereum/wiki/wiki/JSON-RPC)
+It uses the JSON RPC2 interface and can be configured to use any of the endpoints described.
+
+The package currently implements 55 of the API calls documented, the main omissions being ones documented on this page but no longer implemented.
+This encompasses the Solidity interface and most of the SHH whisper interface. As this API is updated the package will
+be updated to suit.
+
+The mapping of Etheruem RPC data types to Dart types is as follows :-
+
+boolean   - bool 
+string    - String
+quantity  - int
+data      - BigInt(see below)
+
+Examples can be found in the examples directory, also the unit test suite contains examples of calling every API interface.
+
+The package was tested against a geth client, version 1.7.2. The API provided should work on older geth clients
+but be aware some calls may return an error of not implemented.
+ 
+The package currently depends on the BigNum package, this package is not currently Dart 2.0 ready and may be 
+superceded by a new BigInt implementation in Dart. This package will be updated approprately as and when any changes are 
+needed.
---
+++ examples/ethereum_example.dart
@@ -0,0 +1,43 @@
+/*
+ * Package : Ethereum
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 03/11/2017
+ * Copyright :  S.Hamblett
+ */
+
+import 'dart:async';
+import 'package:ethereum/ethereum_server_client.dart';
+
+/// Example client usage
+/// Please refer to https://github.com/ethereum/wiki/wiki/JSON-RPC for further details of ports etc.
+
+Future main() async {
+  // Create a client using default parameters
+  final EthereumServerClient client =
+      new EthereumServerClient.withConnectionParameters("localhost");
+
+  // Or, with a specified port for WS working in the browser
+  // new EthereumBrowserWSClient.withConnectionParameters("localhost", 8546);
+
+  // Turn on error printing if needed.
+  client.printError = true;
+
+  // Make an API call, the client is stateless form a connection point of view.
+  final String version = await client.protocolVersion();
+
+  // Check for an error if you think anything is wrong
+  if (version == null) {
+    print("We have errored -> ${client.lastError}");
+  } else {
+    print("We are OK -> ${client.lastError}");
+  }
+
+  // The transaction id increments every time, first call sets it to one
+  print("Transaction Id is -->  ${client.rpcClient.id}");
+
+  // You can have as many clients as you wish, this has its own transaction id
+  final EthereumServerClient client2 =
+      new EthereumServerClient.withConnectionParameters("localhost");
+
+  client2.printError = true;
+}
