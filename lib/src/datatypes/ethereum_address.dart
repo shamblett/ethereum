d074e681f6cc061daf1128e06fb976c0236b4427
Steve Hamblett <steve.hamblett@linux.com>
2019-01-14 13:35:47 +0000

    Issue 5

--- lib/src/datatypes/ethereum_address.dart
+++ lib/src/datatypes/ethereum_address.dart
@@ -11,20 +11,54 @@ part of ethereum;
 
 /// Ethereum address as a byte array, 20 bytes long
 class EthereumByteAddress {
-  /// The length of an Ethereum address in bytes
-  static const int addressByteLength = 20;
-
-  ByteData _data = ByteData(addressByteLength);
-
   /// If the length is greater than addressByteLength then only addressByteLength
   /// are taken. If the length is less than addressByteLength the size is padded
   /// to addressByteLength with 0's
-  EthereumByteAddress(ByteData data) {}
+  EthereumByteAddress(ByteData data) {
+    _setData(data);
+  }
 
   /// If the length is greater than addressByteLength then addressByteLength
   /// are taken. If the length is less than addressByteLength the size is padded
   /// to addressByteLength with 0's. Each int must be < 255, if not it is discarded.
-  EthereumByteAddress.fromIntList(List<int> data) {}
+  EthereumByteAddress.fromIntList(List<int> data) {
+    final ByteData tmp = ByteData(data.length);
+    for (int i = 0; i < tmp.lengthInBytes; i++) {
+      tmp.setUint8(i, data[i]);
+    }
+    _setData(tmp);
+  }
+
+  /// The length of an Ethereum address in bytes
+  static const int addressByteLength = 20;
+
+  ByteData _data;
+
+  @override
+  String toString() => _data.toString();
+
+  @override
+  bool operator ==(dynamic other) =>
+      identical(this, other) ||
+      other.runtimeType == runtimeType && other._data == _data;
+
+  @override
+  int get hashCode => _data.hashCode;
+
+  void _setData(ByteData data) {
+    if (data.lengthInBytes == addressByteLength) {
+      _data = data;
+    } else {
+      if (data.lengthInBytes > addressByteLength) {
+        _data = data.buffer.asByteData(0, addressByteLength - 1);
+      } else {
+        _data = ByteData(addressByteLength);
+        for (int i = 0; i < data.lengthInBytes; i++) {
+          _data.setUint8(i, data.getUint8(i));
+        }
+      }
+    }
+  }
 }
 
 /// The address data type
@@ -41,6 +75,7 @@ class EthereumAddress {
     _convertToBigInt(val);
   }
 
+  /// From a byte address
   EthereumAddress.fromByteAddress(EthereumByteAddress val) {}
 
   /// From a
